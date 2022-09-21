a717024bb428ef79e0fedb2a53684156b423d656
gabmor38 <gabriela.morenor@gmail.com>
2022-09-21 16:21:38 -0400

    created  A-Z and # Array

--- src/webparts/reactMyGroups/components/AZNavigation/AZNavigation.tsx
+++ src/webparts/reactMyGroups/components/AZNavigation/AZNavigation.tsx
@@ -5,15 +5,28 @@ import  styles from './AZNavigation.module.scss';
 
 export class AZNavigation extends React.Component<IAZNavigationProps> {
   public render(): React.ReactElement<IAZNavigationProps> {
-    let arrayAtoZ = () => {
-      return Array.apply(null, {length:26}).map((num, index) => String.fromCharCode(65 + index));
-    };
 
-    let abcChars: string = arrayAtoZ();
-    console.log(abcChars);
+  //Create # symbol array
+    let numberArray = () => {
+      return Array.apply(null, {length:1}).map((num: any, index: number) => String.fromCharCode(35 + index));
+    };
+    let numSym  = numberArray();
+
+  // Create the A-Z Array
+   let arrayAtoZ = () => {
+     return Array.apply(null, {length:26}).map((num: any, index: number) => String.fromCharCode(65 + index));
+   };
+   let abcChars = arrayAtoZ();
+
+  // Combine both ABC and # symbol arrays
+   const combinedIndex = [...abcChars, ...numSym];
+
+   console.log(combinedIndex);
 
     return (
-      <div  className={styles.charContainer}></div>
+      <div  className={styles.charContainer}>
+        items={combinedIndex}
+      </div>
     );
   }
 }
--- src/webparts/reactMyGroups/components/AZNavigation/IAZNavigationProps.ts
+++ src/webparts/reactMyGroups/components/AZNavigation/IAZNavigationProps.ts
@@ -1,5 +1,5 @@
 export interface IAZNavigationProps {
-
+  Letters:string;
   selectedIndexKey: string;
   onIndexSelect: (index: string) => void;
   onSearch: (searchQuery: string) => void;
