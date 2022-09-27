a4ef9f4e9520ca59a637114579c391c6999073c4
gabmor38 <gabriela.morenor@gmail.com>
2022-09-27 08:18:05 -0400

    removed comments

--- src/webparts/reactMyGroups/components/AZNavigation/IAZNavigationProps.ts
+++ src/webparts/reactMyGroups/components/AZNavigation/IAZNavigationProps.ts
@@ -1,19 +1,7 @@
 export interface IAZNavigationProps {
 
   selectedLetter: string;
-  // onIndexSelect: (index: string) => void;
-  // onSearch: (searchQuery: string) => void;
-  // onClearSearch: () => void;
-
-
   onClickEvent:(letter: string) => void;
-
-  // currentPage: number;
-  // nextButtonLabel: string;
-  // previousButtonLabel: string;
-  // nextButtonAriaLabel?: string;
-  // previousButtonAriaLabel?: string;
-
 
 }
 
